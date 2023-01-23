e5bce58f58676c5172b21c2020b4e32ad1e0ac5e
hi16matsuda <ae21h-matsuda@g.kumamoto-nct.ac.jp>
2023-01-24 01:08:52 +0900

    test

--- src/main.py
+++ src/main.py
@@ -1,31 +1,52 @@
-import discord
 import os
+import sys
+import time
+import io
+import discord
+from urllib import request
+from PIL import Image
+from natsort import natsorted
+from subprocess import CREATE_NO_WINDOW
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome import service as fs
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions
 
-# 自分のBotのアクセストークンに置き換えてください
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-desktop-notifications')
+options.add_argument("--disable-extensions")
+options.add_argument('--lang=ja')
+options.add_argument("--proxy-server='direct://'")
+options.add_argument("--proxy-bypass-list=*")
+mobile_emulation = { "deviceName": "Pixel 5" }
+options.add_experimental_option("mobileEmulation", mobile_emulation)
+driver_path = '.\\chromedriver.exe'
+chrome_service = fs.Service(executable_path=driver_path) 
+chrome_service.creationflags = CREATE_NO_WINDOW
+driver = webdriver.Chrome(service=chrome_service, options=options)
+driver.implicitly_wait(60)
+driver.maximize_window()
+wait = WebDriverWait(driver, 1000)
+
 TOKEN = os.environ['DISTOKEN']
 
-# 接続に必要なオブジェクトを生成
 client = discord.Client(intents=discord.Intents.all())
 
-# 起動時に動作する処理
 @client.event
 async def on_ready():
+	user = client.get_user(483645421894500352)
+	await user.send(file=discord.File("sample.png"))
 
-	ch_name = "bottest"
+def Login():
+    url = 'https://www.rakuten.co.jp/'
+    driver.get(url)
+    driver.save_screenshot('sample.png')
 
-	for channel in client.get_all_channels():
-		if channel.name == ch_name:
-			await channel.send("AM8時前後になってたらうれしいなぁ")
-
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '!test':
-        await message.channel.send('test')
-
-# Botの起動とDiscordサーバーへの接続
+Login()
 client.run(TOKEN)
